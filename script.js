014e8124d1412678d5cf9cea71aed7bb28712a46
Abdirino <abdirinoahash@gmail.com>
2022-12-23 11:47:55 +0300

    responsive nav finish

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // nav sec
-const navToggler = document.querySelector(".toggle");
+const navToggler = document.querySelector(".nav-toggler");
 
 navToggler.addEventListener("click", navToggle);
 
 function navToggle() {
     navToggler.classList.toggle("active");
-    const nav = document.querySelector(".navbar");
+    const nav = document.querySelector(".nav");
     nav.classList.toggle("open");
     if(nav.classList.contains("open")){
-        nav.style.maxHeight = nav.scrollHeight + "px"
+        nav.style.maxHeight = nav.scrollHeight + "px";
     }else{
         nav.removeAttribute("style");
     }
