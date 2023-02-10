9cd377632b44856cf9f1a7fb359cbf0a4b3b70e6
Abdirino <abdirinoahash@gmail.com>
2023-02-10 07:48:51 +0300

    kpr hi mouseover effect

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // MEMOIZATION IN JAVASCRIPT
 let memoizedValue = {};
 
-function multiBy5 (num){
-    if(num in memoizedValue){
-        console.log(memoizedValue[num]);
-    }else{
-        memoizedValue[num] = num * 5;
-        console.log(memoizedValue[num]);
-    }
+function multiBy5(num) {
+  if (num in memoizedValue) {
+    console.log(memoizedValue[num]);
+  } else {
+    memoizedValue[num] = num * 5;
+    console.log(memoizedValue[num]);
+  }
 }
 
 multiBy5(7);
@@ -19,7 +19,6 @@ multiBy5(7);
 multiBy5(7);
 multiBy5(7);
 multiBy5(7);
-
 
 // nav sec
 const navToggler = document.querySelector(".nav-toggler");
@@ -76,33 +75,66 @@ const handleHateClick = () => {
 // carmel img hover animation
 const track = document.getElementById("img-track");
 
-window.onmousedown = e => {
-    track.dataset.mouseDownAt = e.clientX;
-}
+window.onmousedown = (e) => {
+  track.dataset.mouseDownAt = e.clientX;
+};
 
 window.onmouseup = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage=track.dataset.percentage;
-}
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+};
 
-window.onmousemove = e => {
-    if(track.dataset.mouseDownAt === "0") return;
+window.onmousemove = (e) => {
+  if (track.dataset.mouseDownAt === "0") return;
 
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-          maxDelta = window.innerWidth / 2;
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+    maxDelta = window.innerWidth / 2;
 
-    const percentage = (mouseDelta / maxDelta) * -100,
-          nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;       
+  const percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
 
-    Math.min(nextPercentage, 0);
-    Math.max(nextPercentage, -100);
+  Math.min(nextPercentage, 0);
+  Math.max(nextPercentage, -100);
 
-    track.dataset.percentage = nextPercentage;
+  track.dataset.percentage = nextPercentage;
 
-    track.style.transform = `translate(${nextPercentage}%, -50%)`;
+  track.style.transform = `translate(${nextPercentage}%, -50%)`;
 
-    for(const image of track.getElementsByClassName("image")) {
-        image.style.objectPosition = `${nextPercentage + 100} 50%`;
-    }
-}
+  for (const image of track.getElementsByClassName("image")) {
+    image.style.objectPosition = `${nextPercentage + 100} 50%`;
+  }
+};
 // end carmel img hover animation
+
+// kpr section
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.getElementById("h1").onmouseover = event => {
+
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    // event.target.innerText = event.target.dataset.value;
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+
+      if(iteration >= event.target.dataset.value.length) {
+        clearInterval(interval)
+      };
+
+      iteration += 1 / 3;
+  }, 30);
+};
+// end kpr section
